691eb926116457942c8e7fe179c1b795007b338a
Raghul. D <raghul.darni@gmail.com>
2026-08-08 11:55:01 +0530

    Update Exp3.py

--- Lab exp/Unit 4/Exp3.py
+++ Lab exp/Unit 4/Exp3.py
@@ -1,31 +1,38 @@
-def assembly_line(a1, a2, t1, t2, e1, e2, x1, x2):
-    n = len(a1)
+def three_assembly_lines(lines, transfer, dependencies):
+    n = len(lines[0])
+    k = len(lines)
 
-    dp1 = [0] * n
-    dp2 = [0] * n
+    dp = [[float('inf')] * n for _ in range(k)]
 
-    dp1[0] = e1 + a1[0]
-    dp2[0] = e2 + a2[0]
+    for line in range(k):
+        dp[line][0] = lines[line][0]
 
-    for i in range(1, n):
-        dp1[i] = min(
-            dp1[i - 1] + a1[i],
-            dp2[i - 1] + t2[i - 1] + a1[i]
-        )
+    for station in range(1, n):
+        for line in range(k):
+            for prev in range(k):
+                dp[line][station] = min(
+                    dp[line][station],
+                    dp[prev][station - 1] +
+                    transfer[prev][line] +
+                    lines[line][station]
+                )
 
-        dp2[i] = min(
-            dp2[i - 1] + a2[i],
-            dp1[i - 1] + t1[i - 1] + a2[i]
-        )
-
-    return min(dp1[-1] + x1, dp2[-1] + x2)
+    return min(dp[line][n - 1] for line in range(k))
 
 
-a1 = [7, 9, 3, 4, 8, 4]
-a2 = [8, 5, 6, 4, 5, 7]
-t1 = [2, 3, 1, 3, 4]
-t2 = [2, 1, 2, 2, 1]
-e1, e2 = 2, 4
-x1, x2 = 3, 2
+lines = [
+    [5, 9, 3],
+    [6, 8, 4],
+    [7, 6, 5]
+]
 
-print("Minimum processing time:", assembly_line(a1, a2, t1, t2, e1, e2, x1, x2))
+transfer = [
+    [0, 2, 3],
+    [2, 0, 4],
+    [3, 4, 0]
+]
+
+dependencies = [(0, 1), (1, 2)]
+
+print("Minimum production time:",
+      three_assembly_lines(lines, transfer, dependencies))
